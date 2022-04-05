5edaf106fade64a4cb7dd91d44f49c503ec3a755
artemiosdev <flyboroda@gmail.com>
2022-04-05 19:48:12 +0300

    Fix errors in code

--- examples/Generator of progression.c
+++ examples/Generator of progression.c
@@ -1,23 +1,21 @@
-
 #include <stdio.h>
 
-main ()
+int main(int argc, char* argv[])
 {
-  int start, stop, step;
-  printf ("Generator of progression.\n" "Enter start, stop, step:");
+    int start, stop, step;
+    printf("Generator of progression.\n"
+           "Enter start, stop, step:");
+    scanf("%d%d%d", &start, &stop, &step);
 
-  scanf ("%d%d%d", &start, &stop, &step);
-
-  int sign = (step > 0)? +1: -1;
-
-  int x = start;
-
-  while (sign*x < sign*stop)
+    int sign = (step > 0)? +1: -1;
+    int x = start;
+    while (sign*x < sign*stop)
     {
-      printf ("x = %d\n", x);
-      x += step;
+        printf("x = %d\n", x);
+        x += step;
     }
-      printf ("After: x = %d\n", x);
-      return 0;
-    
+
+    printf("After: x = %d\n", x);
+
+    return 0;
 }
