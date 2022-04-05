ea39c4f9fc9a074f642d778242fdd0b1549ac843
artemiosdev <flyboroda@gmail.com>
2022-04-05 19:40:58 +0300

    Update notes, add images, add examples and links on course

---
+++ examples/Generator of progression.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+main ()
+{
+  int start, stop, step;
+  printf ("Generator of progression.\n" "Enter start, stop, step:");
+
+  scanf ("%d%d%d", &start, &stop, &step);
+
+  int sign = (step > 0)? +1: -1;
+
+  int x = start;
+
+  while (sign*x < sign*stop)
+    {
+      printf ("x = %d\n", x);
+      x += step;
+    }
+      printf ("After: x = %d\n", x);
+      return 0;
+    
+}
---
+++ examples/infinite loop for.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main()
+{
+    unsigned int i;
+    for (i = 10; i >= 0; --i) {
+        printf("%d\n", i);
+    }
+
+}
